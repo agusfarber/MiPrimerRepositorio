15123859bbffebf82c27cd2d2aec512bb3aeaba4
Agustin Farber <agusfarber@gmail.com>
2024-06-22 10:30:21 -0300

    Mi primer Hola Mundo en PHP

---
+++ PHP/hello_world.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "Hola Mundo \n";
+
+//Comentario de una linea
+
+/*
+Comentario de 
+múltiples
+lineas
+*/ 
+
+$my_string = "Esto es una cadena de texto\n";
+echo $my_string;
+
+echo gettype($my_string); //Muestra el tipo de dato (en este caso string)
+
+
+
+
+?>
